4d52112a66ed44460f0233e2c1a81737ef8b3a91
fa-rehmani <fatemeh.r3366@gmail.com>
2017-06-11 14:52:41 +0430

    header file for login.cpp user login

---
+++ SourceFiles/UserInfo/login.h
@@ -0,0 +1,20 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include "user.h"
+
+
+class login
+{
+public:
+    login(userList ul);
+};
+
+class signup
+{
+public:
+    signup(userList ul);
+
+
+};
+#endif // LOGIN_H
